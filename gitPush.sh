28f0ea9619ca7bc5261424ff92838d0232a39b07
Wenxiang Chen--Laptop <chenwx.ustc@gmail.com>
2012-07-13 22:02:41 -0600

    fix a bug in gitPush.sh

--- gitPush.sh
+++ gitPush.sh
@@ -1,2 +1,2 @@
 git push --all git@github.com:qzcwx/homepage-source-code.git
-git push --tagsgit@github.com:qzcwx/homepage-source-code.git
+git push --tags git@github.com:qzcwx/homepage-source-code.git
